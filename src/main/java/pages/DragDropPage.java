9d14bd3111154b1d459c50489e9a3897343e282e
Surovtseva Diana <dia.surovtseva@gmail.com>
2020-08-27 11:57:03 +0300

    added assert, change on new URL

--- src/main/java/pages/DragDropPage.java
+++ src/main/java/pages/DragDropPage.java
@@ -1,47 +1,38 @@
 package pages;
+
 import org.openqa.selenium.By;
-import org.openqa.selenium.TimeoutException;
-import org.openqa.selenium.WebDriver;
-import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
-import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 import utils.WebDriverFactory;
-import static org.testng.Assert.assertTrue;
+
 
 public class DragDropPage {
-//    private WebDriver driver = null;
-    public String firstFigure;
-    public String firstFigure1;
+
+    public WebElement element1;
+    public WebElement element2;
 
 
-    public void navigateTo(){
-        WebDriverFactory.getDriver().get("http://the-internet.herokuapp.com/drag_and_drop");
+    public void navigateTo() {
+        WebDriverFactory.getDriver().get("http://demo.guru99.com/test/drag_drop.html");
     }
-    public void figureIsVisible(){
-        WebDriverWait wait = new WebDriverWait(WebDriverFactory.getDriver(), 10);
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='column-a']"))).isDisplayed();
+
+    public void startPositionIs() {
+        element1 = WebDriverFactory.getDriver().findElement(By.xpath("//*[@id='bank']//*[@class='placeholder']"));
     }
-    public void firstFigureWasNamed(){
-       firstFigure =  WebDriverFactory.getDriver().findElement(By.xpath("//*[@id='columns']//*[1]//*[1]")).getText();
-        System.out.println("First figure on start is " + firstFigure);
-    }
-    public void dragNDrop(){
+
+    public void dragNDrop() {
         //Actions class method to drag and drop
         Actions builder = new Actions(WebDriverFactory.getDriver());
-        WebElement from = WebDriverFactory.getDriver().findElement(By.id("column-a"));
-        WebElement to = WebDriverFactory.getDriver().findElement(By.id("column-b"));
+        WebElement from = WebDriverFactory.getDriver().findElement(By.id("credit2"));
+        WebElement to = WebDriverFactory.getDriver().findElement(By.id("bank"));
         //Perform drag and drop
         builder.dragAndDrop(from, to).perform();
-
-
     }
 
-
-//    public void firstFigureWasNamed1(){
-//        firstFigure1 = WebDriverFactory.getDriver().findElement(By.xpath("//*[@id='column-a']")).getText();
-//    }
+    public void finalPositionIs() {
+        element2 = WebDriverFactory.getDriver().findElement(By.xpath("//*[@id='bank']//*[contains(text(),'BANK')]"));
+    }
 
 }
--- src/test/java/DragDropTest.java
+++ src/test/java/DragDropTest.java
@@ -1,6 +1,3 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.Sleeper;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.DragDropPage;
@@ -24,15 +21,10 @@ public class DragDropTest {
     @Test
     public void dragDropFigure() {
         dragDropPage.navigateTo();
-        dragDropPage.figureIsVisible();
-        dragDropPage.firstFigureWasNamed();
+        dragDropPage.startPositionIs();
         dragDropPage.dragNDrop();
-
-
-//        dragDropPage.firstFigureWasNamed1();
-
-//        assertTrue(dragDropPage.firstFigure != dragDropPage.firstFigure1);
-
+        dragDropPage.finalPositionIs();
+        assertTrue(dragDropPage.element1 != dragDropPage.element2);
     }
 
 }
